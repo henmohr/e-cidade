727b601be70344adfd380d51763f4cebc6580062
henmohr <henriquemohr@redes.ufsm.br>
2026-02-18 09:36:23 -0300

    Refactor dbforms situacao and contabil legacy request handling

--- dbforms/db_contabil.php
+++ dbforms/db_contabil.php
@@ -1,4 +1,4 @@
-<?
+<?php
 /*
  *     E-cidade Software Publico para Gestao Municipal                
  *  Copyright (C) 2009  DBselller Servicos de Informatica             
@@ -28,14 +28,32 @@
 require(__DIR__ . "/../libs/db_stdlib.php");
 require(__DIR__ . "/../libs/db_conecta.php");
 include(__DIR__ . "/../libs/db_sessoes.php");
-parse_str($HTTP_SERVER_VARS['QUERY_STRING']);
+$legacyQueryParams = [];
+parse_str((string) ($_SERVER['QUERY_STRING'] ?? ''), $legacyQueryParams);
+foreach ($legacyQueryParams as $legacyKey => $legacyValue) {
+  if (!isset($$legacyKey)) {
+    $$legacyKey = $legacyValue;
+  }
+}
 if(!isset($arg)) {
-  $str = explode("\?",$HTTP_SERVER_VARS['QUERY_STRING']);
+  $str = explode("\?", (string) ($_SERVER['QUERY_STRING'] ?? ''));
   $str1 = base64_decode($str[0]);
-  $str2 = base64_decode($str[1]);
+  $str2 = base64_decode($str[1] ?? '');
 //  echo "$str1<br>$str2";
-  parse_str($str1);
-  parse_str($str2);  
+  $legacyDecodedQuery1 = [];
+  parse_str((string) $str1, $legacyDecodedQuery1);
+  foreach ($legacyDecodedQuery1 as $legacyKey => $legacyValue) {
+    if (!isset($$legacyKey)) {
+      $$legacyKey = $legacyValue;
+    }
+  }
+  $legacyDecodedQuery2 = [];
+  parse_str((string) $str2, $legacyDecodedQuery2);
+  foreach ($legacyDecodedQuery2 as $legacyKey => $legacyValue) {
+    if (!isset($$legacyKey)) {
+      $$legacyKey = $legacyValue;
+    }
+  }  
 }
 
 
@@ -64,10 +82,10 @@ if(isset($retorno)) {
 }
 //$arg = explode("==",$arg);
 //$argaux = explode("==",$argaux);
-if(empty($HTTP_POST_VARS["filtro"]))
-  $HTTP_POST_VARS["filtro"] = $arg;
+if(empty($_POST["filtro"]))
+  $_POST["filtro"] = $arg;
 else
-  $arg = $HTTP_POST_VARS["filtro"];
+  $arg = $_POST["filtro"];
   
   if( $argaux !=""){
      $chave = $campoaux;
@@ -129,16 +147,16 @@ else
 <td align="center" nowrap>
 
 <form name="form5" method="post">
-  <input type="text" name="filtro" value="<?=@$HTTP_POST_VARS['filtro']?>" onBlur="window.focus();">
-  <input type="hidden" name="arg" value="<?=@$HTTP_POST_VARS['arg']?>">
+  <input type="text" name="filtro" value="<?=@$_POST['filtro']?>" onBlur="window.focus();">
+  <input type="hidden" name="arg" value="<?=@$_POST['arg']?>">
   <input type="submit" name="procurar" value="Procurar">
 </form>
 </td>
 </tr>
 <tr>
 <td align="center">
-<?
-db_lov($sql,15,"db_caixa.php?".base64_encode("campo=$campo&campoaux=$campoaux"),$HTTP_POST_VARS["filtro"]);
+<?php
+db_lov($sql,15,"db_caixa.php?".base64_encode("campo=$campo&campoaux=$campoaux"),$_POST["filtro"]);
 ?>
 </td>
 </tr>
